556cb1f898b82085e3a768a3d290a24caabae112
mnlg <mnlg@users.noreply.github.com>
2022-03-01 16:34:43 +0100

    Use map for config tests

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,55 +10,34 @@ func TestLoadConfig(t *testing.T) {
 		t.Errorf("Error loading config file: %v\n", err)
 	}
 
-	if config.Server != "example.com" {
-		t.Error("Server != example.com")
-	}
-
 	if config.ServerPort != 9000 {
 		t.Error("ServerPort != 9000")
-	}
-
-	if config.DB.Server != "dbexample.com" {
-		t.Error("DB.Server != dbexample.com")
 	}
 
 	if config.DB.Port != 3306 {
 		t.Error("DB.Port != 3306")
 	}
 
-	if config.DB.User != "root" {
-		t.Error("DB.User != root")
+	m := []struct {
+		input string
+		want  string
+	}{
+		{config.Server, "example.com"},
+		{config.DB.Server, "dbexample.com"},
+		{config.DB.User, "root"},
+		{config.DB.Pass, "root"},
+		{config.DB.Name, "test"},
+		{config.CrawlerAgent, "testing"},
+		{config.Stripe.Key, "stripe_key"},
+		{config.Stripe.Secret, "stripe_secret"},
+		{config.Stripe.WebhookSecret, "webhook_secret"},
+		{config.Stripe.AdvancedPriceId, "price_id"},
+		{config.Stripe.ReturnURL, "http://localhost:9000"},
 	}
 
-	if config.DB.Pass != "root" {
-		t.Error("DB.Pass != root")
-	}
-
-	if config.DB.Name != "test" {
-		t.Error("DB.Name != test")
-	}
-
-	if config.CrawlerAgent != "testing" {
-		t.Error("CrawlerAgent != testing")
-	}
-
-	if config.Stripe.Key != "stripe_key" {
-		t.Error("Stripe.Key != stripe_key")
-	}
-
-	if config.Stripe.Secret != "stripe_secret" {
-		t.Error("Stripe.Secret != stripe_secret")
-	}
-
-	if config.Stripe.WebhookSecret != "webhook_secret" {
-		t.Error("Stripe.WebhookSecret != webhook_secret")
-	}
-
-	if config.Stripe.AdvancedPriceId != "price_id" {
-		t.Error("Stripe.AdvancedPriceId != price_id")
-	}
-
-	if config.Stripe.ReturnURL != "http://localhost:9000" {
-		t.Error("Stripe.ReturnURL != http://localhost:9000")
+	for _, v := range m {
+		if v.input != v.want {
+			t.Errorf("%s != %s\n", v.input, v.want)
+		}
 	}
 }

25d934eb58683a49943dbc3e5b7fefa354c49ac4
StJudeWasHere <StJudeWasHere@users.noreply.github.com>
2022-03-28 09:22:20 +0200

    Use ParseGlob to load templates

--- internal/helper/renderer.go
+++ internal/helper/renderer.go
@@ -30,18 +30,7 @@ func (r *Renderer) RenderTemplate(w http.ResponseWriter, t string, v *PageView) 
 	var templates = template.Must(
 		template.New("").Funcs(template.FuncMap{
 			"trans": r.trans,
-		}).ParseFiles(
-			"web/templates/head.html",
-			"web/templates/footer.html",
-			"web/templates/home.html",
-			"web/templates/issues_view.html",
-			"web/templates/issues.html",
-			"web/templates/charts.html",
-			"web/templates/project_add.html",
-			"web/templates/resources.html",
-			"web/templates/signin.html",
-			"web/templates/signup.html",
-		))
+		}).ParseGlob("web/templates/*.html"))
 
 	err := templates.ExecuteTemplate(w, t+".html", v)
 	if err != nil {

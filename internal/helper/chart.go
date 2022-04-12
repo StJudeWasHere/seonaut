95f730b4ab9a0a4d74f13a42c5c5a10c498e55b2
StJudeWasHere <StJudeWasHere@users.noreply.github.com>
2022-04-12 19:48:10 +0200

    Use js to show the charts

--- internal/helper/chart.go
+++ internal/helper/chart.go
@@ -1,19 +1,16 @@
 package helper
 
 import (
-	"strconv"
-
 	"github.com/stjudewashere/seonaut/internal/issue"
 )
 
 const (
-	chartLimit = 4
+	chartLimit = 5
 )
 
 type ChartItem struct {
-	Key     string
-	Value   int
-	Percent int
+	Key   string
+	Value int
 }
 
 type Chart []ChartItem
@@ -28,17 +25,8 @@ func NewChart(c issue.CountList) Chart {
 
 	for _, i := range c {
 		ci := ChartItem{
-			Key:     i.Key,
-			Value:   i.Value,
-			Percent: int(float64(i.Value) / float64(total) * 100),
-		}
-
-		if ci.Percent == 0 {
-			ci.Percent = 1
-		}
-
-		if ci.Percent > 97 {
-			ci.Percent = 100 - (len(c) - 1)
+			Key:   i.Key,
+			Value: i.Value,
 		}
 
 		chart = append(chart, ci)
@@ -48,27 +36,10 @@ func NewChart(c issue.CountList) Chart {
 		chart[chartLimit-1].Key = "Other"
 		for _, v := range chart[chartLimit:] {
 			chart[chartLimit-1].Value += v.Value
-			chart[chartLimit-1].Percent += v.Percent
 		}
 
 		chart = chart[:chartLimit]
 	}
 
 	return chart
-}
-
-func (c Chart) GetChart(c1, c2, c3, c4 string) string {
-	colors := []string{c1, c2, c3, c4}
-	var s string
-	last := 0
-	for i, v := range c {
-		s = s + colors[i] + " " + strconv.Itoa(last) + "% " + strconv.Itoa(last+v.Percent) + "%,"
-		last = last + v.Percent
-	}
-
-	if len(s) > 0 {
-		return s[0 : len(s)-1]
-	}
-
-	return s
 }

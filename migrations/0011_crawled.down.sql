5753064f5c0052a2b356f6393d03bdf63e494a4f
StJudeWasHere <StJudeWasHere@users.noreply.github.com>
2022-05-02 11:29:42 +0200

    Add columns to crawls table

--- migrations/0011_crawled.down.sql
+++ migrations/0011_crawled.down.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `pagereports` DROP COLUMN `crawled`;
+
+ALTER TABLE `crawls` DROP COLUMN `blocked_by_robotstxt`;
+ALTER TABLE `crawls` DROP COLUMN `noindex`;
--- migrations/0011_crawled.up.sql
+++ migrations/0011_crawled.up.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `pagereports` ADD COLUMN `crawled` tinyint NOT NULL DEFAULT '1';
+
+ALTER TABLE `crawls` ADD COLUMN `blocked_by_robotstxt` int NOT NULL DEFAULT 0;
+ALTER TABLE `crawls` ADD COLUMN `noindex` int NOT NULL DEFAULT 0;
